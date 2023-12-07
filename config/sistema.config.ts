58c3947992300eb749937003d1f02ae39e675901
Diego Alves <diegocarmo.profissional@gmail.com>
2023-12-07 17:25:51 -0300

    adding github actions

--- config/sistema.config.ts
+++ config/sistema.config.ts
@@ -1,7 +1,7 @@
 export default {
   port: process.env.PORT,
   DirFotos: "/home/diego/Documentos/imoveis_orientado_testes_solid/src/publico/fotos/",
-  //DirFotos: "/var/www/imoveis_backend_solid/src/publico/fotos/",
+  //DirFotos: "/var/www/imoveis_backend/src/publico/fotos/",
 
   //urlApi: "http://138.197.84.102:3000"
   urlApi: "http://localhost:3000"
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata'
-import dotenv from 'dotenv'; dotenv.config({ path: '../../.env' })
+import dotenv from 'dotenv'; dotenv.config()
 import { createConnection } from 'typeorm'
 import { config } from '../../config/pg-config'
 
