ae6821831a43f0e7f543db8e7740c8f93628f7b1
Diego Alves <diegocarmo.profissional@gmail.com>
2023-12-08 09:18:38 -0300

    implementando github workflow

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,18 @@ import path from 'path';
 import dotenv from 'dotenv'; dotenv.config({ path: path.join(__dirname, '..', '..', '.env') })
 import { createConnection } from 'typeorm'
 import { config } from '../../config/pg-config'
+import https from 'https';
+import fs from 'fs'
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/imootour.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/imootour.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/imootour.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
 
 
 
@@ -15,7 +27,11 @@ createConnection(config).then(async (connection) => {
 
 
   const app = (await import('./config/app')).default
-  app.listen(3000, () => {
+
+  const httpsServer = https.createServer(credentials, app);
+
+
+  httpsServer.listen(3000, () => {
 
   }
   )
