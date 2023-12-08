67d43d476b2ea181c862bdf0aebc73bf3652e93b
Diego Alves <diegocarmo.profissional@gmail.com>
2023-12-08 11:23:15 -0300

    implementando github workflow

--- config/sistema.config.ts
+++ config/sistema.config.ts
@@ -1,6 +1,6 @@
 export default {
   port: process.env.PORT,
-  DirFotos: "/var/www/publico/fotos",
+  DirFotos: "/var/www/publico/fotos/",
   //DirFotos: "/var/www/imoveis_backend/src/publico/fotos/",
 
   //urlApi: "http://138.197.84.102:3000"
